dd8623c96decb76f010aa26839a4ce29fa80ec3f
Ifan <ifan@Ifans-MacBook-Air.local>
2025-11-15 13:51:03 +0700

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Native PHP To-Do</title>
+    <title>To-Do</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
